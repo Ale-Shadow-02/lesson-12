0ad9ca3b4cb594011cd4f8bc4326e74ef8abe2b6
Alexandr_Dvoryadkin <dvoryadkin66@mail.ru>
2019-09-29 15:15:12 +0400

    Урок 16 на проверку калькулятор

--- js/script.js
+++ js/script.js
@@ -258,10 +258,9 @@ window.addEventListener('DOMContentLoaded', function () {
     });
   };
   mouseHover();
-
+  // Проверка инпутов на число
   const inputValid = () => {
     const input = document.querySelectorAll('.calc-block > input');
-    console.log('input: ', input);
     input.forEach((elem) => {
       elem.addEventListener('input', () => {
         elem.value = elem.value.replace(/\D/g, '');
@@ -270,5 +269,49 @@ window.addEventListener('DOMContentLoaded', function () {
   };
   inputValid();
 
+  // Калькулятор
+
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      totalValue = document.getElementById('total');
+
+      const countSum = () => {
+        let total = 0,
+        countValue = 1,
+        dayValue = 1;
+        const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+        if (calcCount.value > 1) {
+          countValue += (calcCount.value -1) / 10;
+        }
+
+        if (calcDay.value && calcDay.value < 5) {
+          dayValue *= 2;
+        } else if (calcDay.value && calcDay.value < 10) {
+          dayValue *= 1.5;
+        }
+        
+        if (typeValue && squareValue) {
+          total = price * typeValue * squareValue * countValue * dayValue;
+        } 
+        
+        totalValue.textContent = Math.ceil(total);
+      };
+    
+      calcBlock.addEventListener('change', (event) => {
+        const target = event.target;
+        if (target.matches('select') || target.matches('input')) {
+          countSum();
+        }
+    });
+
+
+  };
+  calc(100);
 
 });
