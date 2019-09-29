382f1ec77a198990ffbcb2eee3e2bd2503ae7ecc
Alexandr_Dvoryadkin <dvoryadkin66@mail.ru>
2019-09-29 16:23:44 +0400

    исправлена ошибка при расчете на 0

--- js/script.js
+++ js/script.js
@@ -279,35 +279,37 @@ window.addEventListener('DOMContentLoaded', function () {
       calcDay = document.querySelector('.calc-day'),
       totalValue = document.getElementById('total');
 
-      const countSum = () => {
-        let total = 0,
+    const countSum = () => {
+      let total = 0,
         countValue = 1,
         dayValue = 1;
-        const typeValue = calcType.options[calcType.selectedIndex].value,
+      const typeValue = calcType.options[calcType.selectedIndex].value,
         squareValue = +calcSquare.value;
 
-        if (calcCount.value > 1) {
-          countValue += (calcCount.value -1) / 10;
-        }
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
 
-        if (calcDay.value && calcDay.value < 5) {
-          dayValue *= 2;
-        } else if (calcDay.value && calcDay.value < 10) {
-          dayValue *= 1.5;
-        }
-        
-        if (typeValue && squareValue) {
-          total = price * typeValue * squareValue * countValue * dayValue;
-        } 
-        
-        totalValue.textContent = Math.ceil(total);
-      };
-    
-      calcBlock.addEventListener('change', (event) => {
-        const target = event.target;
-        if (target.matches('select') || target.matches('input')) {
-          countSum();
-        }
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue && +calcCount.value && +calcDay.value) {
+        console.log('calcDay.value: ', calcDay.value);
+        console.log('calcCount.value: ', calcCount.value);
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      totalValue.textContent = Math.ceil(total);
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
     });
 
 
